26603266f1370d51c597dd5baad01cea6457a866
DESKTOP-RGBOF48\harsh <harsha.v.js@gmail.com>
2020-07-06 15:18:03 -0800

    Initial commit

--- Matrix multiplication.py
+++ Matrix multiplication.py
@@ -1,21 +1,19 @@
 def matrixm(a,b):
     ans = [[0, 0],
            [0, 0]]
-    for x in range(0,2):
-        for y in range(0,2):
-            for z in range(0,2):
+    for x in range(len(a)):
+        for y in range(len(b[0])):
+            for z in range(len(b)):
                 ans[x][y] = ans[x][y] + a[x][z] * b[z][y]
+    return ans
 
-    for x in range(0, 2):
-        for y in range(0, 2):
-
-            print(ans[x][y], end=" ")
-        print("\n", end="")
-
-
-a = [[1, 2],
-     [3, 4]]
+a =[[1, 2],
+    [3, 4]]
 b = [[5, 6],
      [7, 8]]
-matrixm(a,b)
+c = matrixm(a,b)
+for x in range(0, 2):
+    for y in range(0, 2):
+        print(c[x][y], end=" ")
+        print("\n", end="")
 
